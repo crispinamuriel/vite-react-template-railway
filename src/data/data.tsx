c9feb6171533bc9e93b695ab7105b8d2119528e8
Crispina Muriel <crispinamuriel@gmail.com>
2025-07-11 16:27:17 -0700

    add new project

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -2,13 +2,13 @@ export const Info = [
     {
       title: 'Technical Skills',
       content:
-        'JavaScript (ES6+), TypeScript, HTML5, CSS, React.js, Gatsby, Next.js, Redux.js, Node.js, Express.js, PostgreSQL, Python, Django, SQL, RDBMS, Babel, Webpack, Git, GitHub, AJAX, RESTful APIs, JSON, WebSockets, Socket.io, Webhooks, Jest, Jasmine, Mocha, Chai, Enzyme, Cypress, Selenium, LocalStorage, Full Stack Development, Heroku, Google OAuth, Agile, SCRUM, JIRA, Confluence, Pair Programming, UI/UX, Linux, Bash, Browserify, Vue.js, Vuetify, Ember.js',
+        'JavaScript (ES6+), TypeScript, GraphQL, React.js, Next.js, Vite, Gatsby, HTML5, CSS, Redux.js, Node.js, Express.js, PostgreSQL, Python, Django, FastAPI, SQL, RDBMS, Babel, Webpack, Git, GitHub, AJAX, RESTful APIs, JSON, WebSockets, Socket.io, Webhooks, Jest, Jasmine, Mocha, Chai, Enzyme, Cypress, Selenium, PyTest, LocalStorage, Full Stack Development, Heroku, Google OAuth, Agile, SCRUM, JIRA, Confluence, Pair Programming, UI/UX, Linux, Bash, Browserify, Vue.js, Vuetify, Ember.js',
       imgUrl: 'https://images.unsplash.com/photo-1633356122544-f134324a6cee?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
       title: 'Professional Background',
       content:
-        'My passions are Tech, Learning, and Education. I have received credentials to teach elementary school in California and New York. As a user of educational apps in my classroom, I was always inspired at how technology seemed to motivate my class to learn. I loved my time spent working with children and am proud to have contributed to the EdTech field that reaches many children in a meaningful way. \n \n For the past three years I have immersed myself in coding and constructing full stack applications for various clients. Working with JavaScript, TypeScript, and Python has been an exhilarating journey, allowing me to unleash my creativity within the realm of web application development. I have been actively participating in diverse projects as a Full Stack Developer. I notably collaborated on an educational app, a fintech app, a health tech app, signup and payment flows, marketing websites, and an insurance processing app.',
+        'My passions are Tech, Learning, and Education. I have received credentials to teach elementary school in California and New York. As a user of educational apps in my classroom, I was always inspired at how technology seemed to motivate my class to learn. I loved my time spent working with children and am proud to have contributed to the EdTech field that reaches many children in a meaningful way. \n \n For the past three years I have immersed myself in coding and constructing full stack applications for various clients. Working with JavaScript, TypeScript, and Python has been an exhilarating journey, allowing me to unleash my creativity within the realm of web application development. I have been actively participating in diverse projects as a Full Stack Developer. I notably collaborated on an educational app, a fintech app, two health tech apps, signup and payment flows, marketing websites, and an internal insurance underwriting app.',
       imgUrl: 'https://images.unsplash.com/photo-1604933834413-4cbe62737214?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
@@ -20,7 +20,7 @@ export const Info = [
     {
       title: 'Professional Interests',
       content:
-        "I've attended a few AWS Loft events and single-day 'bootcamps' about Machine Learning, Internet of Things, and Blockchain. I am interested in learning more about these topics. If you feel like you want to discuss anything tech, let's connect on LinkedIn and chat! The next languages I'm looking to learn next are Rust, C#, and Java. I hope to learn a little more about AWS, GraphQL, Docker, and Kubernetes.",
+        "I've attended a few AWS Loft events and single-day 'bootcamps' about Machine Learning, Internet of Things, and Blockchain. I am interested in learning more about these topics. If you feel like you want to discuss anything tech, let's connect on LinkedIn and chat! The next languages I'm looking to learn next are Rust, GoLang, and Java. I hope to learn a little more about AWS, GCP, Docker, and Kubernetes.",
       imgUrl: 'https://images.unsplash.com/photo-1593076819667-745edc34bb40?q=80&w=3430&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
@@ -33,20 +33,28 @@ export const Info = [
   export const ProjectsData = [
     {
       id: 1,
+      title: "Jasra's House and Petsitting",
+      content: "Local business webpage",
+      imageUrl: "jasra-petsitting.png",
+      projectUrl: "https://jasra.netlify.app",
+      gitHubUrl: "https://github.com/crispinamuriel/jasra-webpage",
+    },
+    {
+      id: 2,
       title: "Calibrate Membership",
       content: "Sign-up and payment flow for Calibrate members",
       imageUrl: "calibrate.png",
       projectUrl: "https://apply.joincalibrate.com/oymr",
     },
     {
-      id: 2,
+      id: 3,
       title: "Join Calibrate",
       content: "Calibrate marketing website",
       imageUrl: "calibrate2.png",
       projectUrl: "https://www.joincalibrate.com",
     },
     {
-      id: 3,
+      id: 4,
       title: "EnCoMPASS",
       content: "An educational app for students & teacher collaboration",
       imageUrl: "encompass.png",
@@ -54,7 +62,7 @@ export const Info = [
       gitHubUrl: "https://github.com/crispinamuriel/encompass",
     },
     {
-      id: 4,
+      id: 5,
       title: "Codewords",
       content: "A multiplayer game based on the board game Codenames",
       imageUrl: "codewords.png",
@@ -62,7 +70,7 @@ export const Info = [
       gitHubUrl: "https://github.com/crispinamuriel/Codewords",
     },
     {
-      id: 5,
+      id: 6,
       title: "LoveLaces",
       content: "An e-commerce full stack application for women’s shoes",
       imageUrl: "Shoes.png",
@@ -70,11 +78,11 @@ export const Info = [
       gitHubUrl: "https://github.com/crispinamuriel/Lovelaces",
     },
     {
-      id: 6,
+      id: 7,
       title: "Recipe App",
       content: "A single page application to store user recipes",
       imageUrl: "recipe.png",
-      projectUrl: "https://muriel-recipe-app.netlify.com/",
+      projectUrl: "https://muriel-recipe-app.netlify.app",
       gitHubUrl: "https://github.com/crispinamuriel/recipe-app",
     },
   ];
